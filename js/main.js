f5e9da012e5c15bec5b769585621011258be878c
Nallely House <nallelyht@gmail.com>
2017-08-06 01:53:52 -0500

    cambiando menu

--- js/main.js
+++ js/main.js
@@ -1,17 +1,7 @@
-document.querySelector( "#nav-toggle" )
-	.addEventListener( "click", function() {
-	this.classList.toggle( "active" );
-});
-
-
 $(document).ready(function() {
 	$(".flag").lettering();
 	new WOW().init();
-	$('#mainNav').affix({
-		offset: {
-			top: 100
-		}
-	});
+
 	var s,
 			spanizeLetters = {
 				settings: {
@@ -30,4 +20,9 @@ $(document).ready(function() {
 				},
 			};
 	spanizeLetters.init();
+
+	$('.menu-burger, .menu-items').on('click', function() {
+		$('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+		$('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
+	});
 });
